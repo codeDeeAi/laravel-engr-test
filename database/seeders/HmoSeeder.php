0b235cb3adbc3b3adb30243332e1a6e94f1e7446
Adeola Bada <dunsin.bada@gmail.com>
2024-09-21 08:25:01 +0100

    [ADD] added api for hmo suggestions, [MOD] hmoseeder class

--- database/seeders/HmoSeeder.php
+++ database/seeders/HmoSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\BatchRuleEnum;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -9,10 +10,10 @@ use Illuminate\Support\Facades\DB;
 class HmoSeeder extends Seeder
 {
     private $hmos = [
-        ['name'=>'HMO A', 'code'=> 'HMO-A'],
-        ['name'=>'HMO B', 'code'=> 'HMO-B'],
-        ['name'=>'HMO C', 'code'=> 'HMO-C'],
-        ['name'=>'HMO D', 'code'=> 'HMO-D'],
+        ['name' => 'HMO A', 'code' => 'HMO-A', 'email' => 'email_hmo_a@mailinator.com', 'batch_rule' => BatchRuleEnum::ENCOUNTER_DATE->value],
+        ['name' => 'HMO B', 'code' => 'HMO-B', 'email' => 'email_hmo_b@mailinator.com', 'batch_rule' => BatchRuleEnum::SUBMISSION_DATE->value],
+        ['name' => 'HMO C', 'code' => 'HMO-C', 'email' => 'email_hmo_c@mailinator.com', 'batch_rule' => BatchRuleEnum::SUBMISSION_DATE->value],
+        ['name' => 'HMO D', 'code' => 'HMO-D', 'email' => 'email_hmo_d@mailinator.com', 'batch_rule' => BatchRuleEnum::SUBMISSION_DATE->value],
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\v1\Providers\HmoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+/**
+ * API V1 routes
+ * 
+ * @note - These routes are versioned with the prefix for simplicity purposes 
+ */
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('/hmo/suggestions', [HmoController::class, 'search']);
+});
