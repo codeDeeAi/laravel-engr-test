ad78cd856687f2e144cb94899ff5d47e37759f0f
Adeola Bada <dunsin.bada@gmail.com>
2024-09-21 07:12:29 +0100

    [MOD] updated Hmo model attributes and cast

--- app/Models/Hmo.php
+++ app/Models/Hmo.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use App\Enums\BatchRuleEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Hmo extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'code',
+        'email',
+        'batch_rule',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'batch_rule' => BatchRuleEnum::class,
+        ];
+    }
 }
